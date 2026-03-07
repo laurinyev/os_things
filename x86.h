441e08f280cc19f7e030d2c338ff31bfa80e7760
laurinyev <retek1544@gmail.com>
2026-03-07 14:50:24 +0100

    [x86.h] added a syscall registers struct

--- x86.h
+++ x86.h
@@ -35,7 +35,6 @@ inline void outl(uint16_t addr, uint32_t val){
 }
 
 //======================= MSR ========================
-
 #define MSR_IA32_EFER   0xC0000080
 #define MSR_IA32_STAR   0xC0000081
 #define MSR_IA32_LSTAR  0xC0000082
@@ -84,6 +83,25 @@ typedef struct {
     uint64_t rsp;
     uint64_t ss;
 } __attribute__((packed)) registers_t;
+
+typedef struct {
+    uint64_t r15;
+    uint64_t r14;
+    uint64_t r13;
+    uint64_t r12;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+    uint64_t rdi;
+    uint64_t rsi;
+    uint64_t rbp;
+    uint64_t rbx;
+    uint64_t rdx;
+    uint64_t rax;
+
+    uint64_t rflags;
+    uint64_t rip;
+} __attribute__((packed)) registers_syscall_t;
 
 inline void lidt(void* idtr) {
     asm volatile("lidt (%0)":: "r"(idtr));
